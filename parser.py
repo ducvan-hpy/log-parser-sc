101d9150891ab5bc1ee9b743eea81cc89e1bd505
Duc Van Trang <trang.ducvan@icloud.com>
2024-04-26 20:19:55 +1000

    fix: delete extra space before type hints

--- parser.py
+++ parser.py
@@ -5,59 +5,59 @@ import json
 
 class Condition(ABC):
     @abstractmethod
-    def is_valid(lc : int, line : str) -> bool:
+    def is_valid(lc: int, line: str) -> bool:
         pass
 
     @abstractmethod
-    def render(lc : int, line : str) -> None:
+    def render(lc: int, line: str) -> None:
         pass
 
 class Multiple5Condition(Condition):
-    def is_valid(lc : int, line : str) -> bool:
+    def is_valid(lc: int, line: str) -> bool:
         return lc % 5 == 0
 
-    def render(lc : int, line : str) -> None:
+    def render(lc: int, line: str) -> None:
         print("{} : Multiple de 5".format(lc))
 
 
 class ContainsDollarCondition(Condition):
-    def is_valid(lc : int, line : str) -> bool:
+    def is_valid(lc: int, line: str) -> bool:
         return "$" in line
 
-    def render(lc : int, line : str) -> None:
+    def render(lc: int, line: str) -> None:
         line = line.replace(" ", "_")
         print("{} : {}".format(lc, line))
 
 
 class DotEndingCondition(Condition):
-    def is_valid(lc : int, line : str) -> bool:
+    def is_valid(lc: int, line: str) -> bool:
         try:
             return line[-1] == "."
         except IndexError:
             return False
 
-    def render(lc : int, line : str) -> None:
+    def render(lc: int, line: str) -> None:
         print("{} : {}".format(lc, line))
 
 
 class CurlBracketStartingCondition(Condition):
-    def is_valid(lc : int, line : str) -> bool:
+    def is_valid(lc: int, line: str) -> bool:
         try:
             return line[0] == "{"
         except IndexError:
             return False
 
-    def render(lc : int, line : str) -> None:
+    def render(lc: int, line: str) -> None:
         data = json.loads(line)
         data["pair"] = lc % 2 == 0
         print("{} : {}".format(lc, json.dumps(data, ensure_ascii=False)))
 
 
 class DefaultCondition(Condition):
-    def is_valid(lc : int, line : str) -> bool:
+    def is_valid(lc: int, line: str) -> bool:
         return True
 
-    def render(lc : int, line : str) -> None:
+    def render(lc: int, line: str) -> None:
         print("{} : Rien à afficher".format(lc))
 
 
