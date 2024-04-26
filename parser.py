4c085e11f64c2ae580be0d7590386dac2fd02e67
Duc Van Trang <trang.ducvan@icloud.com>
2024-04-26 11:43:23 +1000

    feat: add initial parser
    With Multiple5Condition, ContainsDollarCondition, DotEndingCondition, CurlBracketStartingCondition and DefaultCondition.

---
+++ parser.py
@@ -0,0 +1,68 @@
+#! /usr/bin/env python3
+
+import json
+
+class Multiple5Condition:
+    def is_valid(lc : int, line : str) -> bool:
+        return lc % 5 == 0
+
+    def render(lc : int, line : str) -> None:
+        print("{} : Multiple de 5".format(lc))
+
+
+class ContainsDollarCondition:
+    def is_valid(lc : int, line : str) -> bool:
+        return "$" in line
+
+    def render(lc : int, line : str) -> None:
+        line = line.replace(" ", "_")
+        print("{} : {}".format(lc, line))
+
+
+class DotEndingCondition:
+    def is_valid(lc : int, line : str) -> bool:
+        return line[-1] == "."
+
+    def render(lc : int, line : str) -> None:
+        print("{} : {}".format(lc, line))
+
+
+class CurlBracketStartingCondition:
+    def is_valid(lc : int, line : str) -> bool:
+        return line[0] == "{"
+
+    def render(lc : int, line : str) -> None:
+        data = json.loads(line)
+        data["pair"] = lc % 2 == 0
+        print("{} : {}".format(lc, json.dumps(data)))
+
+
+class DefaultCondition:
+    def is_valid(lc : int, line : str) -> bool:
+        return True
+
+    def render(lc : int, line : str) -> None:
+        print("{} : Rien à afficher".format(lc))
+
+
+if __name__ == "__main__":
+    condition_classes = [
+        Multiple5Condition,
+        ContainsDollarCondition,
+        DotEndingCondition,
+        CurlBracketStartingCondition,
+        DefaultCondition,
+    ]
+
+    filename = "data.log"
+
+    lc = 0
+
+    with open(filename, "r") as f:
+        for line in f:
+            line = line.rstrip()
+            for cc in condition_classes:
+                if cc.is_valid(lc, line):
+                    cc.render(lc, line)
+                    break
+            lc += 1
